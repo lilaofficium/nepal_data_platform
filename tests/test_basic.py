15684be8c71d6ce39e2839764231ef6f0dc2988f
lila Yadav <lila.yadavofficium@gmail.com>
2026-06-26 21:10:42 +0545

    test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,12 +2,10 @@ def test_that_passes():
     """This test will always pass"""
     assert 1 + 1 == 2
 
-def test_mongo_uri_exists():
-    """Check that MONGO_URI environment variable is set"""
+def test_mongo_uri_exists(): 
     import os
     mongo_uri = os.getenv("MONGO_URI")
     assert mongo_uri is not None, "MONGO_URI is missing!"
 
-def test_intentional_failure():
-    """THIS WILL FAIL ON PURPOSE — to test our pipeline catches errors"""
-    assert 1 + 1 == 5, "Math is broken!"  
+def test_intentional_failure(): 
+    assert 1 + 1 == 2, "Math is broken!"  
